94a8b2240bd7ff1b412f1417979084a8ca4a5260
Sashriya <sashriya.murali@gmail.com>
2025-12-19 19:31:18 +0530

    Updated commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import girl from '../assets/girl.jpg'
 import {motion as Motion} from 'framer-motion'
-import Aboutsec from '../components/Aboutsec'
+import AboutSec from '../components/AboutSec'
 
 function About() {
   return (
     <div className='flex justify-between items-center bg-black md:p-10 p-5 gap-20'>
-      <Aboutsec />
+      <AboutSec />
       <Motion.img
       key='image' initial={{opacity:0, x:100}} animate={{opacity:1, x:0}}
       transition={{delay:0.3, duration:0.2, ease:'easeOut'}}
