79b11c285f61b8aa0206886002ed5f05c99f169b
Sashriya <sashriya.murali@gmail.com>
2025-12-19 22:46:32 +0530

    Updated commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -17,10 +17,10 @@ function Profile() {
       <h2 className='md:text-5xl text-4xl font-bold italic'>Hi! I'm <span className='text-teal-500'> Sashriya M </span></h2>
       <p className='text-2xl font-semibold italic'>
         <TypeAnimation sequence={[
-            "I am an Web Developer", 3000,
-            "I am an App Developer", 3000,
-            "I am an AI Engineer", 3000,
-            "I am an Creative Designer", 3000,
+            "Web Developer", 3000,
+            "App Developer", 3000,
+            "AI Engineer", 3000,
+            "Creative Designer", 3000,
         ]}
         wrapper='span' speed={50} repeat={Infinity} />
       </p>
